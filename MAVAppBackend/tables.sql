ce017cbaa69e2301d2f2ea2c4fc5f0b20dd37ac8
DanielSharp01 <DanielSharp01@gmail.com>
2018-06-12 01:37:01 +0200

    Added sql to generate DB, readme modified

---
+++ MAVAppBackend/tables.sql
@@ -0,0 +1,73 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.11 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             9.5.0.5196
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for mavapp
+CREATE DATABASE IF NOT EXISTS `mavapp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `mavapp`;
+
+-- Dumping structure for table mavapp.stations
+CREATE TABLE IF NOT EXISTS `stations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `norm_name` varchar(255) NOT NULL,
+  `lat` double NOT NULL,
+  `long` double NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `norm_name` (`norm_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2050 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+-- Dumping structure for table mavapp.trains
+CREATE TABLE IF NOT EXISTS `trains` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `elvira_id` varchar(50) NOT NULL,
+  `number` varchar(50) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `type` varchar(50) DEFAULT NULL,
+  `number_type` varchar(50) DEFAULT NULL,
+  `delay` int(11) NOT NULL,
+  `delay_reason` text,
+  `lat` double DEFAULT NULL,
+  `long` double DEFAULT NULL,
+  `enc_polyline` text NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `elvira_id` (`elvira_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+-- Dumping structure for table mavapp.train_stations
+CREATE TABLE IF NOT EXISTS `train_stations` (
+  `train_id` int(11) NOT NULL,
+  `number` int(11) NOT NULL,
+  `station` int(11) DEFAULT NULL,
+  `mav_name` varchar(255) NOT NULL,
+  `int_distance` int(11) NOT NULL,
+  `distance` double NOT NULL,
+  `position_accuracy` tinyint(2) NOT NULL COMMENT '0 - missing, 1 - integer, 2 - precise',
+  `arrive` timestamp NOT NULL,
+  `depart` timestamp NOT NULL,
+  `arrive_actual` timestamp NOT NULL,
+  `depart_actual` timestamp NOT NULL,
+  `arrived` tinyint(1) NOT NULL,
+  `platform` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`train_id`,`number`),
+  KEY `FK_train_stations_stations` (`station`),
+  CONSTRAINT `FK_train_stations_stations` FOREIGN KEY (`station`) REFERENCES `stations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_train_stations_trains` FOREIGN KEY (`train_id`) REFERENCES `trains` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- README.md
+++ README.md
@@ -13,24 +13,10 @@ This project is a JSON API for accessing and processing MÁV's (Hungarian Railwa
 
 - [x] Station extraction
 - [x] Train station deduction when Google data is unavailable
-- [ ] Legally correct approach to Google Places API data extraction (dynamic approach instead static storage)
 - [ ] Trains from ROUTE API
 - [ ] Trains from STATION API
 - [x] Train static data extraction
-- [ ] Train dynamic data extraction
+- [x] Train dynamic data extraction
 - [ ] Closest station (from GPS coordinates)
 - [ ] Deduce train travelled with (from stream of GPS coordinates for a couple seconds)
-- [ ] MySQL data storage
-
-
-## Notice
-
-The application uses an `all_stations.txt` which I can't share legally as it's using Google Places API data. You have to place this file next to the executable (for now, it will probably move into an SQL database).
-
-`all_stations.txt` has this format:
-```
-Station name (can include spaces)|LATITUDE (double), LONGITUDE (double)
-eg. Szeged|46.239722, 20.143064
-```
-
-The extraction code used is shared in the GoogleMapsExtract.cs source file, but you do have to get creative to actually get the full list. For that I share a comparison code with the MAV Station data in MAVAPI.cs. When you only see foreign sounding stations returned, that's when you have won.
+- [x] MySQL data storage
